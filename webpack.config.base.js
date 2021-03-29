ef5b4d88292534398dc7d42a007eb42e7c5f0c99
ling <l13789060033@163.com>
2021-03-29 13:04:42 +0800

    上传

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,4 +13,37 @@ module.exports = {
   }),
 
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        loader: ["style-loader","css-loader","stylus-loader",],
+      },
+      {
+        test: /\.less$/i,
+        loader: ["style-loader","css-loader","less-loader",]
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require('dart-sass'),
+             },
+          },
+        ],
+      },
+    ],
+  },
 };
